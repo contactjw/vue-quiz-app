0f6bbfa94e70c29cd9d8effba5e75497e31f79f2
John West <contactjohnwest@gmail.com>
2023-11-05 17:14:37 -0800

    Add router, views, Question component, and QuizHeader component

---
+++ src/views/QuizView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <QuizHeader />
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue'
+import quizzes from '../data/quizzes.json'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+const route = useRoute()
+
+const quizId = parseInt(route.params.id)
+
+const quiz = quizzes.find((q) => q.id === quizId)
+
+const currentQuestionIndex = ref(0)
+</script>
+
+<style scoped></style>
